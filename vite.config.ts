d9fe9c2ec42cc432406e30cfdcc66ea2836ce615
Mateusz Wadoń <mwadon@gmail.com>
2025-11-27 14:39:43 +0100

    Change build directory

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist"),
+    outDir: path.resolve(import.meta.dirname, "dist/docs/console"),
     emptyOutDir: true,
   },
 });
